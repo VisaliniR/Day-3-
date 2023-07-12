39cd3e4a6b9903cee4ba736cf8ac4564ac495fb7
VisaliniR <132570870+VisaliniR@users.noreply.github.com>
2023-07-12 11:09:04 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+var on_btn = document.getElementById("on");
+var of_btn = document.getElementById("off");
+var img=document.getElementById("bulb");
+function bulb_on(){
+    img.src="./image/bulb-on.jpg";
+}
+
+function bulb_off(){
+    img.src="./image/bulb-off.jpg";
+}
